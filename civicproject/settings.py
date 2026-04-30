93d83597abaabfa189cb1896392b3666113b2c60
ig-4 <indugrg15@gmail.com>
2026-04-30 10:10:05 +0545

    commit

--- civicproject/settings.py
+++ civicproject/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'civicproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 AUTH_USER_MODEL = 'accounts.User'
 

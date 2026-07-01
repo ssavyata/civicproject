285506bbfaa8113bacbf5555bd32ee7ebfab6fda
ig-4 <indugrg15@gmail.com>
2026-07-01 18:00:59 +0545

    changes

--- civicproject/settings.py
+++ civicproject/settings.py
@@ -66,7 +66,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'civicproject.urls'
 
